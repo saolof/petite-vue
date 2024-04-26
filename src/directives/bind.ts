3dcace29db79b5829afde80f9495b92be7c40153
saolof <olof.salberger@gmail.com>
2024-04-26 20:35:17 +0200

    Make bind.ts directive self-contained

--- src/directives/bind.ts
+++ src/directives/bind.ts
@@ -1,14 +1,87 @@
 import { Directive } from '.'
-import {
-  normalizeClass,
-  normalizeStyle,
-  isString,
-  isArray,
-  hyphenate,
-  camelize
-} from '@vue/shared'
 
 const forceAttrRE = /^(spellcheck|draggable|form|list|type)$/
+
+// Normalization:
+
+const camelizeRE = /-(\w)/g
+const hyphenateRE = /\B([A-Z])/g
+const camelize = cacheStringFunction((str: string): string => {
+  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
+})
+const hyphenate = cacheStringFunction((str: string) =>
+  str.replace(hyphenateRE, '-$1').toLowerCase(),
+)
+
+const isArray = Array.isArray
+const isString = (val: unknown): val is string => typeof val === 'string'
+const isObject = (val: unknown): val is Record<any, any> =>
+  val !== null && typeof val === 'object'
+
+const listDelimiterRE = /;(?![^(]*\))/g
+const propertyDelimiterRE = /:([^]+)/
+const styleCommentRE = /\/\*[^]*?\*\//g
+
+type NormalizedStyle = Record<string, string | number>
+
+function parseStringStyle(cssText: string): NormalizedStyle {
+  const ret: NormalizedStyle = {}
+  cssText
+    .replace(styleCommentRE, '')
+    .split(listDelimiterRE)
+    .forEach(item => {
+      if (item) {
+        const tmp = item.split(propertyDelimiterRE)
+        tmp.length > 1 && (ret[tmp[0].trim()] = tmp[1].trim())
+      }
+    })
+  return ret
+}
+
+function normalizeStyle(
+  value: unknown,
+): NormalizedStyle | string | undefined {
+  if (isArray(value)) {
+    const res: NormalizedStyle = {}
+    for (let i = 0; i < value.length; i++) {
+      const item = value[i]
+      const normalized = isString(item)
+        ? parseStringStyle(item)
+        : (normalizeStyle(item) as NormalizedStyle)
+      if (normalized) {
+        for (const key in normalized) {
+          res[key] = normalized[key]
+        }
+      }
+    }
+    return res
+  } else if (isString(value) || isObject(value)) {
+    return value
+  }
+}
+
+function normalizeClass(value: unknown): string {
+  let res = ''
+  if (isString(value)) {
+    res = value
+  } else if (isArray(value)) {
+    for (let i = 0; i < value.length; i++) {
+      const normalized = normalizeClass(value[i])
+      if (normalized) {
+        res += normalized + ' '
+      }
+    }
+  } else if (isObject(value)) {
+    for (const name in value) {
+      if (value[name]) {
+        res += name + ' '
+      }
+    }
+  }
+  return res.trim()
+}
+
+// End of normalization
 
 export const bind: Directive<Element & { _class?: string }> = ({
   el,
