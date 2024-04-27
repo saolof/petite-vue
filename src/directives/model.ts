d9f2fb32532a55ff876b48b2c4f040e2160c271d
saolof <olof.salberger@gmail.com>
2024-04-27 13:31:13 +0200

    Replaced looseequal with == in model.ts
    Made the file feel more vanilla JS ish

--- src/directives/model.ts
+++ src/directives/model.ts
@@ -1,85 +1,5 @@
 import { Directive } from '.'
 
-// LooseEqual
-
-const listen = (
-  el: Element,
-  event: string,
-  handler: any,
-  options?: any
-) => {
-  el.addEventListener(event, handler, options)
-}
-
-const isArray = Array.isArray
-const isDate = (val: unknown): val is Date =>
-  toTypeString(val) === '[object Date]'
-const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
-const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
-
-function looseCompareArrays(a: any[], b: any[]) {
-  if (a.length !== b.length) return false
-  let equal = true
-  for (let i = 0; equal && i < a.length; i++) {
-    equal = looseEqual(a[i], b[i])
-  }
-  return equal
-}
-
-function looseEqual(a: any, b: any): boolean {
-  if (a === b) return true
-  let aValidType = isDate(a)
-  let bValidType = isDate(b)
-  if (aValidType || bValidType) {
-    return aValidType && bValidType ? a.getTime() === b.getTime() : false
-  }
-  aValidType = isSymbol(a)
-  bValidType = isSymbol(b)
-  if (aValidType || bValidType) {
-    return a === b
-  }
-  aValidType = isArray(a)
-  bValidType = isArray(b)
-  if (aValidType || bValidType) {
-    return aValidType && bValidType ? looseCompareArrays(a, b) : false
-  }
-  aValidType = isObject(a)
-  bValidType = isObject(b)
-  if (aValidType || bValidType) {
-    /* istanbul ignore if: this if will probably never be called */
-    if (!aValidType || !bValidType) {
-      return false
-    }
-    const aKeysCount = Object.keys(a).length
-    const bKeysCount = Object.keys(b).length
-    if (aKeysCount !== bKeysCount) {
-      return false
-    }
-    for (const key in a) {
-      const aHasKey = a.hasOwnProperty(key)
-      const bHasKey = b.hasOwnProperty(key)
-      if (
-        (aHasKey && !bHasKey) ||
-        (!aHasKey && bHasKey) ||
-        !looseEqual(a[key], b[key])
-      ) {
-        return false
-      }
-    }
-  }
-  return String(a) === String(b)
-}
-
-function looseIndexOf(arr: any[], val: any): number {
-  return arr.findIndex(item => looseEqual(item, val))
-}
-
-
-// end of LooseEqual
-
-
-
 export const model: Directive<
   HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
 > = ({ el, exp, get, effect, modifiers }) => {
@@ -89,7 +9,7 @@ export const model: Directive<
 
   if (el.tagName === 'SELECT') {
     const sel = el as HTMLSelectElement
-    listen(el, 'change', () => {
+    el.addEventListener('change', () => {
       const selectedVal = Array.prototype.filter
         .call(sel.options, (o: HTMLOptionElement) => o.selected)
         .map((o: HTMLOptionElement) =>
@@ -104,13 +24,13 @@ export const model: Directive<
         const option = sel.options[i]
         const optionValue = getValue(option)
         if (isMultiple) {
-          if (isArray(value)) {
-            option.selected = looseIndexOf(value, optionValue) > -1
+          if (Array.isArray(value)) {
+            option.selected = value.indexOf(optionValue) > -1
           } else {
             option.selected = value.has(optionValue)
           }
         } else {
-          if (looseEqual(getValue(option), value)) {
+          if (getValue(option) == value) {
             if (sel.selectedIndex !== i) sel.selectedIndex = i
             return
           }
@@ -121,12 +41,12 @@ export const model: Directive<
       }
     })
   } else if (type === 'checkbox') {
-    listen(el, 'change', () => {
+    el.addEventListener('change', () => {
       const modelValue = get()
       const checked = (el as HTMLInputElement).checked
-      if (isArray(modelValue)) {
+      if (Array.isArray(modelValue)) {
         const elementValue = getValue(el)
-        const index = looseIndexOf(modelValue, elementValue)
+        const index = modelValue.indexOf(elementValue)
         const found = index !== -1
         if (checked && !found) {
           assign(modelValue.concat(elementValue))
@@ -143,26 +63,25 @@ export const model: Directive<
     let oldValue: any
     effect(() => {
       const value = get()
-      if (isArray(value)) {
+      if (Array.isArray(value)) {
         ;(el as HTMLInputElement).checked =
-          looseIndexOf(value, getValue(el)) > -1
+          value.indexOf(getValue(el)) > -1
       } else if (value !== oldValue) {
-        ;(el as HTMLInputElement).checked = looseEqual(
-          value,
-          getCheckboxValue(el as HTMLInputElement, true)
+        ;(el as HTMLInputElement).checked = (
+          value == getCheckboxValue(el as HTMLInputElement, true)
         )
       }
       oldValue = value
     })
   } else if (type === 'radio') {
-    listen(el, 'change', () => {
+    el.addEventListener('change', () => {
       assign(getValue(el))
     })
     let oldValue: any
     effect(() => {
       const value = get()
       if (value !== oldValue) {
-        ;(el as HTMLInputElement).checked = looseEqual(value, getValue(el))
+        ;(el as HTMLInputElement).checked = (value == getValue(el))
       }
     })
   } else {
@@ -173,14 +92,14 @@ export const model: Directive<
       return val
     }
 
-    listen(el, 'compositionstart', onCompositionStart)
-    listen(el, 'compositionend', onCompositionEnd)
-    listen(el, modifiers?.lazy ? 'change' : 'input', () => {
+    el.addEventListener('compositionstart', onCompositionStart)
+    el.addEventListener('compositionend', onCompositionEnd)
+    el.addEventListener(modifiers?.lazy ? 'change' : 'input', () => {
       if ((el as any).composing) return
       assign(resolveValue(el.value))
     })
     if (trim) {
-      listen(el, 'change', () => {
+      el.addEventListener('change', () => {
         el.value = el.value.trim()
       })
     }
